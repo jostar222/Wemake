4bc8fe2f0730679bdda87984e4d782d7a8aeb083
jostar222 <whtjdgus911@gmail.com>
2025-03-14 23:38:12 +0900

    #3.8 Job Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -6,6 +6,8 @@ import { ProductCard } from "~/features/products/components/product-card";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import { PostCard } from "~/features/community/components/post-card";
 import { IdeaCard } from "~/features/ideas/components/idea-card";
+import { Badge } from "../components/ui/badge";
+import { JobCard } from "~/features/jobs/components/job-card";
 
 export const meta: MetaFunction = () => {
   return [
@@ -83,6 +85,32 @@ export default function HomePage() {
           likesCount={12}
           claimed={index % 2 === 0}
         /> 
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Jobs
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            Find your dream job.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/jobs">Explore all jobs &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({length: 11}).map((_, index) => (
+          <JobCard
+          id="jobId"
+          company="Tesla"
+          companyLogoUrl="https://github.com/facebook.png"
+          companyHq="San Francisco, CA"
+          title="Software Engineer"
+          postedAt="12 hours ago"
+          type="Full-time"
+          positionLocation="Remote"
+          salary="$100,000 - $120,000"
+        />
         ))}
       </div>
     </div>
