8e1461defbb11ec3bdc490852bed1ea5977d79e4
jostar222 <whtjdgus911@gmail.com>
2025-03-17 23:01:45 +0900

    #3.20 Search Page

--- app/features/products/pages/search-page.tsx
+++ app/features/products/pages/search-page.tsx
@@ -1,28 +1,64 @@
-import type { Route } from "../../../+types/features/products/pages/search-page";
-import type { MetaFunction } from "@react-router/types";
+import { data, Form } from "react-router";
+import type { Route } from "./+types/search-page";
+import { z } from "zod";
+import { Hero } from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import ProductPagination from "~/common/components/product-pagination";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
 
-export function meta(): MetaFunction {
+export const meta: Route.MetaFunction = () => {
   return [
-    { title: "Search Products | ProductHunt Clone" },
+    { title: "Search Products | wemake" },
     { name: "description", content: "Search for products" },
   ];
 }
 
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
+
 export function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
-  const query = url.searchParams.get("q") || "";
-
-  return {
-    query,
-    results: [], // Add search logic
-  };
+  const { success, data: parsedData } = paramsSchema.safeParse(
+    Object.fromEntries(url.searchParams)
+  );
+  if (!success) {
+    throw new Error("Invalid params");
+  }
+  console.log(parsedData);
 }
 
 export default function SearchPage({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Search Results</h1>
-      {/* Add search results */}
+    <div className="space-y-10">
+      <Hero 
+          title="Search"
+          subtitle="Search for products by title or description"
+      />
+      <Form className="flex justify-center max-w-screen-sm items-center gap-2 mx-auto">
+        <Input 
+            name="query" 
+            placeholder="Search for products" 
+            className="text-lg"
+        />
+        <Button type="submit">Search</Button>
+      </Form>
+      <div className="space-y-5 w-full max-w-screen-lg mx-auto">
+        {Array.from({length: 11}).map((_, index) => ( 
+              <ProductCard
+                key={`productId-${index}`}
+                id={`productId-${index}`}
+                name="Product Name"
+                description="Product Description"
+                commentsCount={12}
+                viewsCount={12}
+                votesCount={120}
+            />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
 }
