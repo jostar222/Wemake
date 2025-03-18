6a16b98f5144e67c8c5b58f033669cce619b1142
jostar222 <whtjdgus911@gmail.com>
2025-03-18 23:32:49 +0900

    #3.22 Inputs and Selects

---
+++ app/common/components/select-pair.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "./ui/select";
+
+export default function SelectPair({
+  name,
+  required,
+  label,
+  description,
+  placeholder,
+  options,
+}: {
+  name: string;
+  required: boolean;
+  label: string;
+  description: string;
+  placeholder: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
+}) {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="space-y-2 flex flex-col">
+      <Label className="flex flex-col" onClick={() => setOpen(true)}>
+        {label}
+        <small className="text-muted-foreground">{description}</small>
+      </Label>
+      <Select 
+        open={open} 
+        onOpenChange={setOpen} 
+        name={name} 
+        required={required}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  ) 
+}
