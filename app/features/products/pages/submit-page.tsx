3c87752c4f020099c27098584a4398a2a01e9ae6
jostar222 <whtjdgus911@gmail.com>
2025-03-20 22:43:45 +0900

    #3.23 Submit Product Page

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -6,6 +6,9 @@ import { Input } from "~/common/components/ui/input";
 import InputPair from "~/common/components/input-pair";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "~/common/components/ui/select";
 import SelectPair from "~/common/components/select-pair";
+import { useState } from "react";
+import { Button } from "~/common/components/ui/button";
+
 export const meta: Route.MetaFunction = () => {
   return [
     { title: "Submit Product | wemake" },
@@ -13,7 +16,15 @@ export const meta: Route.MetaFunction = () => {
   ];
 }
 
-export default function SubmitPage({ actionData }: Route.ComponentProps) {
+export default function SubmitPage() {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if(event.target.files) {
+      const file = event.target.files[0];
+      setIcon(URL.createObjectURL(file));
+    }
+  };
   return (
     <div className="container mx-auto px-4 py-8">
       <Hero 
@@ -72,6 +83,32 @@ export default function SubmitPage({ actionData }: Route.ComponentProps) {
               { label: "Marketing", value: "marketing" },
             ]}
           />
+          <Button type="submit" className="w-full size=lg">Submit</Button>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <div className="size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ? (
+              <img src={icon} className="object-cover w-full h-full" />
+            ) : null}
+          </div>
+          <Label className="flex flex-col gap-1">
+            Icon{" "}
+            <small className="text-muted-foreground">
+              This is the icon of your product.
+            </small>
+          </Label>
+          <Input type="file" className="w-1/2" onChange={onChange} required name="icon" />
+          <div className="flex flex-col text-xs">
+            <span className="text-muted-foreground">
+              Recommended size: 128x128px
+            </span>
+            <span className="text-muted-foreground">
+              Allowd formats: PNG, JPEG
+            </span>
+            <span className="text-muted-foreground">
+              Maximum size: 1MB
+            </span>
+          </div>
         </div>
       </Form>
     </div>
