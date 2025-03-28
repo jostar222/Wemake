335d2974da65a9ff2955865e567ea9ee608f32d2
jostar222 <whtjdgus911@gmail.com>
2025-03-28 22:51:56 +0900

    #3.38 Teams Page

---
+++ app/features/teams/pages/team-page.tsx
@@ -0,0 +1,9 @@
+import type { Route } from "./+types/team-page";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Team Details | wemake" },
+];
+
+export default function TeamPage() {
+  return <div></div>;
+}
